cc0fff435a6a25aa1955671f37f36b34e247c368
MohdRehan-dev <rehan.sayed2003007@gmail.com>
2025-02-22 08:58:33 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+//  Primitive
+
+//  7 types  : String, Number, Boolean, null, undefined, Symbol, BigINt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+// let userEmail = undefined
+
+// or
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol ('123')
+
+// console.log(id === anotherId); 
+
+// const bigNumber = 5449876541649646n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"] ;
+
+let myObj = {
+    name:"rehan",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof anotherId);
