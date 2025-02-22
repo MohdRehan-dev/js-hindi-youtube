8cc2113765469ab1de7bdf940458f5a9a548fe87
MohdRehan-dev <rehan.sayed2003007@gmail.com>
2025-02-22 14:53:00 +0000

    String Stuff

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,46 @@
+const name = "rehan"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} 
+    and my repo count is ${repoCount}`)
+
+
+    const gameName = new String ('Rehan-hc-com')
+
+    // console.log(gameName[0]);
+    // console.log(gameName.__proto__);
+
+    // console.log(gameName.length);
+    
+    // console.log(gameName.toUpperCase());
+
+    console.log(gameName.charAt(2));
+
+    console.log(gameName.indexOf('e'));
+
+
+
+
+ const newString = gameName.substring(0, 4)
+ console.log(newString);
+
+
+
+ const anotherString = gameName.slice(-7, 4)
+ console.log(anotherString);
+
+ 
+ 
+ const newStringOne = "         rehan        "
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+ 
+const url = "https://rehan.com/rehan%20sayed"
+
+console.log (url.replace ('%20', '-'))
+
+console.log (url.includes('salman'))
+
+console.log(gameName.split ('-'));
